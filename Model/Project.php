f8e6f8a801bd62c6f1918641201cdc9816baf145
Hui Ser Choo <choohuiser@yahoo.com>
2021-03-24 00:49:05 -0400

    update on project update

--- Model/Project.php
+++ Model/Project.php
@@ -27,4 +27,25 @@ class Project
         return $count;
 
     }
+
+    public function updateProject($id,$name, $project_timestamp, $description, $db){
+        $sql = "Update project
+                set name = :name,
+                project_timestamp = :project_timestamp,
+                description = :description
+                WHERE id = :id
+        
+        ";
+
+        $pst =  $db->prepare($sql);
+
+        $pst->bindParam(':name', $name);
+        $pst->bindParam(':project_timestamp', $project_timestamp);
+        $pst->bindParam(':descriptio', $description);
+        $pst->bindParam(':id', $id);
+
+        $count = $pst->execute();
+
+        return $count;
+    }
 }
--- views/update-project.php
+++ views/update-project.php
@@ -1,1 +1,112 @@
 <?php
+
+// Start or resume a session
+session_start();
+
+require("./partials/header.php");
+//require("./partials/sidebar.php");
+insertHeader();
+//insertSidebar();
+//session_start();
+//require_once './newprojectCheck.php';
+require_once '../Model/Project.php';
+require_once '../Model/Database.php';
+require_once '../Model/ProjectOverview.php';
+require_once '../Model/UpdateProject.php';
+
+$name = $project_timestamp = $description = "";
+$p = new Project();
+$projects = $p->getProjects(Database::getDb());
+
+if(isset($_POST['updateProject'])){
+    $id= $_POST['id'];
+
+    $db = Database::getDb();
+
+    $p = new Project();
+    $project = $p->getProjectById($id, $db);
+    
+    $name = $project->name;
+    $project_timestamp = $project->project_timestamp;
+    $description = $project->description;
+
+}
+if(isset($_POST['updProject'])){
+    $id= $_POST['sid'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $program = $_POST['program'];
+
+    $db = Database::getDb();
+    $s = new Student();
+    $count = $s->updateStudent($id, $name, $email, $program, $db);
+
+    if($count){
+       header('Location:  list-students.php');
+    } else {
+        echo "problem";
+    }
+}
+
+if(isset($_POST['addProject'])) {
+    $name = $_POST['project_name'];
+    $project_timestamp = $_POST['project_timestamp'];
+    $description = $_POST['project_description'];
+    
+    $db = Database::getDb();
+    $p = new Project();
+    $projects = $p->addProject($name, $project_timestamp, $description, $db);
+
+}
+    ?>
+    <!--Main Start Here-->
+    <!--Content Start here-->
+    <main>
+        <div class="container container-login text-center my-5">
+            <div class="row justify-content-md-center">
+                <h2>Update Project</h2>
+                <div>
+                    <form id="add_project_form" name="form_add_project" method="POST" action="">
+
+                        <div class="form-group row mb-3">
+                            <label class="col-sm-3 col-form-label" for="project_name">Project Name</label>
+                            <input class="col-sm-9" type="text" name="project_name" id="project_name" placeholder="Please type your project name">
+                            <span style="color:red;"><?= isset($projectNameErr) ? $projectNameErr : ''; ?></span>
+                        </div>
+
+
+                        <div class="form-group row mb-3">
+                            <label class="col-sm-3 col-form-label" for="project_timestamp">Start Date</label>
+                            <input class="col-sm-9" type="date" name="project_timestamp" id="project_timestamp" placeholder="Please select the project's start date">
+                            <span style="color:red;"><?= isset($projectTimestampErr) ? $projectTimestampErr : ''; ?></span>
+                        </div>
+
+                        <div class="form-group row mb-3">
+                            <div class="label"><label class="col-form-label" for="project_description">Project Description</label>
+                                <textarea class="form-control" name="project_description" id="project_description" rows="6" placeholder="Please provide the details of the project"></textarea>
+                                <span style="color:red;"><?= isset($projectDescErr) ? $projectDescErr : ''; ?></span>
+                            </div>
+
+
+                            <div class="form-group row mb-3">
+                                <label class="col-sm-3 col-form-label" for="member">Member(s)</label>
+                                <input class="col-sm-9" type="" name="member[]" id="member[]" placeholder="Please Select your member">
+                                <span style="color:red;"><?= isset($members_err) ? $members_err : ''; ?></span>
+                            </div>
+
+                            <div class="form-group my-5 text-center">
+                                <div>
+                                    <input type="submit" name ="addProject" class="btn btn-primary btn-lg" value="Create" >
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php
+
+    require("./partials/footer.php");
+    insertFooter();
+
+    ?>
